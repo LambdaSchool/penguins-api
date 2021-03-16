ee033013c6156cf3381bb739f0de2e401ccbfd4d
Ryan Allred <ryanleeallred@gmail.com>
2021-03-16 03:32:23 -0600

    more tests

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ def predict_species(penguin: Penguin):
 
     """
     penguin = penguin.to_df()
-    return penguin
+    return penguin['bill_length_mm'][0]
     # return penguin['bill_depth_mm']['0']    
     # if penguin['bill_depth_mm']['0'] < 16.5:
     #     return "Gentoo"
